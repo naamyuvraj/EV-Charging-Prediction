1b19120509f5b18ecd1bbc2c7caed6aa211d1678
Yatin81 <yatinbisht81@gmail.com>
2026-02-23 18:55:11 +0530

    Fixed the model training one

--- EV_Charging_Demand_Prediction/src/train_model.py
+++ EV_Charging_Demand_Prediction/src/train_model.py
@@ -1,12 +1,6 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-import numpy as np
-import joblib
-from preprocess import load_and_preprocess
+rom sklearn.linear_model import LinearRegression
 
-def train():
-    df_long = load_and_preprocess("../data/volume.csv")
-
+def train_model(df_long):
     X = df_long[['hour', 'day_of_week', 'month', 'is_weekend']]
     y = df_long['volume_kwh']
 
@@ -18,13 +12,4 @@ def train():
     model = LinearRegression()
     model.fit(X_train, y_train)
 
-    predictions = model.predict(X_test)
-
-    mae = mean_absolute_error(y_test, predictions)
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-
-    print("MAE:", mae)
-    print("RMSE:", rmse)
-
-    joblib.dump(model, "ev_model.pkl")
-    print("Model saved as ev_model.pkl")
+    return model, X_test, y_test
