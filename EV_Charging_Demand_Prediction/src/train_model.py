328870ea1213ca8e0f1f82401922e18b3776e817
Yatin81 <yatinbisht81@gamil.com>
2026-02-23 21:46:03 +0530

    fixed the prediction graph

--- EV_Charging_Demand_Prediction/src/train_model.py
+++ EV_Charging_Demand_Prediction/src/train_model.py
@@ -1,13 +1,28 @@
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 def train_model(df_long):
-    X = df_long[['hour', 'day_of_week', 'month', 'is_weekend']]
+    df_long['lag_1'] = df_long['volume_kwh'].shift(1)
+    df_long = df_long.dropna()
+    df_long['hour_sin'] = np.sin(2 * np.pi * df_long['hour'] / 24)
+    df_long['hour_cos'] = np.cos(2 * np.pi * df_long['hour'] / 24)
+
+    df_long['dow_sin'] = np.sin(2 * np.pi * df_long['day_of_week'] / 7)
+    df_long['dow_cos'] = np.cos(2 * np.pi * df_long['day_of_week'] / 7)
+
+    X = df_long[['hour_sin', 'hour_cos',
+                 'dow_sin', 'dow_cos',
+                 'month', 'is_weekend','lag_1']]
+
     y = df_long['volume_kwh']
 
     split_index = int(len(X) * 0.8)
 
-    X_train, X_test = X[:split_index], X[split_index:]
-    y_train, y_test = y[:split_index], y[split_index:]
+    X_train = X.iloc[:split_index]
+    X_test  = X.iloc[split_index:]
+
+    y_train = y.iloc[:split_index]
+    y_test  = y.iloc[split_index:]
 
     model = LinearRegression()
     model.fit(X_train, y_train)
